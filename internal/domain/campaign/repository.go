e0c722f556432421cfa214412085b3eb76594c39
loveo2d <loveo2d@naver.com>
2025-06-13 17:58:09 +0900

    add: 유스케이스에서 사용할 리포지토리 및 모델(엔티티) 정의

--- internal/domain/campaign/repository.go
+++ internal/domain/campaign/repository.go
@@ -1,1 +1,47 @@
 package campaign
+
+import "github.com/jackc/pgx/v5"
+
+type CampaignRepository struct {
+	tx pgx.Tx
+}
+
+func NewCampaignRepository(tx pgx.Tx) *CampaignRepository {
+	return &CampaignRepository{tx: tx}
+}
+
+func (r *CampaignRepository) Create(campaign *Campaign) (*Campaign, error) {
+	return nil, nil
+}
+
+func (r *CampaignRepository) Get(campaignID int32) (*Campaign, error) {
+	return nil, nil
+}
+
+func (r *CampaignRepository) GetWithLock(campaignID int32) (*Campaign, error) {
+	return nil, nil
+}
+
+func (r *CampaignRepository) Update(campaign *Campaign) (*Campaign, error) {
+	return nil, nil
+}
+
+func (r *CampaignRepository) Delete(campaignID int32) error {
+	return nil
+}
+
+type CampaignScheduleRepository struct {
+	tx pgx.Tx
+}
+
+func NewCampaignScheduleRepository(tx pgx.Tx) *CampaignScheduleRepository {
+	return &CampaignScheduleRepository{tx: tx}
+}
+
+func (r *CampaignScheduleRepository) Create(campaignSchedule *CampaignSchedule) (*CampaignSchedule, error) {
+	return nil, nil
+}
+
+func (r *CampaignScheduleRepository) Get(campaignScheduleID int32) (*CampaignSchedule, error) {
+	return nil, nil
+}
--- internal/domain/coupon/repository.go
+++ internal/domain/coupon/repository.go
@@ -1,1 +1,21 @@
 package coupon
+
+import (
+	"github.com/jackc/pgx/v5"
+)
+
+type CouponRepository struct {
+	tx pgx.Tx
+}
+
+func NewCouponRepository(tx pgx.Tx) *CouponRepository {
+	return &CouponRepository{tx: tx}
+}
+
+func (r *CouponRepository) Create(coupon *Coupon) (*Coupon, error) {
+	return nil, nil
+}
+
+func (r *CouponRepository) Get(couponID int32) (*Coupon, error) {
+	return nil, nil
+}
