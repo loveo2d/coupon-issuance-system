26baecd6e0f0a1cc13e88a8a2d672f6a97d162d8
loveo2d <loveo2d@naver.com>
2025-06-13 16:45:01 +0900

    update: DB 커넥션 설정 함수 추가

--- internal/infra/db/config.go
+++ internal/infra/db/config.go
@@ -1,1 +1,13 @@
 package db
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const databaseURL = "postgresql://postgres:postgres@localhost:5432/coupon_issuance_system"
+
+func NewDB() (*pgxpool.Pool, error) {
+	return pgxpool.New(context.Background(), databaseURL)
+}
