bd5b5c2325e06b3259f27ce475a60e6e48b8a33e
loveo2d <loveo2d@naver.com>
2025-06-13 16:16:42 +0900

    update: 유스케이스 Input/Output 명세 추가

--- internal/app/campaign/get/usecase.go
+++ internal/app/campaign/get/usecase.go
@@ -1,17 +1,30 @@
 package campaign_get
 
+import (
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
 type Input struct {
-	CampaignID int32 `json:"campaign_id"`
+	CampaignID int32
 }
 
 type Output struct {
+	CampaignId    int32
+	Title         string
+	CouponRemains int32
+	BeginAt       time.Time
 }
 
 type GetCampaignUC struct {
+	db *pgxpool.Pool
 }
 
-func New() *GetCampaignUC {
-	return &GetCampaignUC{}
+func New(db *pgxpool.Pool) *GetCampaignUC {
+	return &GetCampaignUC{
+		db: db,
+	}
 }
 
 func (uc *GetCampaignUC) Execute(input Input) (*Output, error) {
--- internal/app/coupon/issue/usecase.go
+++ internal/app/coupon/issue/usecase.go
@@ -1,18 +1,28 @@
 package coupon_issue
 
+import (
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
 type Input struct {
-	CampaignID int32 `json:"campaign_id"`
+	CampaignID int32
 }
 
 type Output struct {
-	CouponCode string `json:"coupon_code"`
+	CouponID   int64
+	CouponCode string
+	CampaignID int32
+	IssuedAt   time.Time
 }
 
 type IssueCouponUC struct {
+	db *pgxpool.Pool
 }
 
-func New() *IssueCouponUC {
-	return &IssueCouponUC{}
+func New(db *pgxpool.Pool) *IssueCouponUC {
+	return &IssueCouponUC{db: db}
 }
 
 func (uc *IssueCouponUC) Execute(input Input) (*Output, error) {
