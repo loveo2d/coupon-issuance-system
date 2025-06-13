9ae56b52ad9b90337d607d3d85e4a1f2977af424
loveo2d <loveo2d@naver.com>
2025-06-13 22:30:36 +0900

    update: 난수를 포함하는 Coupon 생성 도메인서비스 추가

--- internal/domain/coupon/service.go
+++ internal/domain/coupon/service.go
@@ -2,6 +2,9 @@ package coupon
 
 import (
 	"context"
+	"fmt"
+	"math/rand"
+	"time"
 
 	"github.com/loveo2d/CouponIssuanceSystem/internal/infra/db"
 )
@@ -15,5 +18,25 @@ func NewCouponService(db db.DB) *CouponService {
 }
 
 func (s *CouponService) IssueCoupon(ctx context.Context, campaignId int32) (*Coupon, error) {
-	return nil, nil
+	couponRepo := NewCouponRepository(s.db)
+
+	couponModel, errCoupon := couponRepo.Create(ctx, &Coupon{
+		Code:       generateCouponCode(),
+		CampaignId: campaignId,
+		IssuedAt:   time.Now(),
+	})
+	if errCoupon != nil {
+		return nil, errCoupon
+	}
+	return couponModel, nil
+}
+
+func generateCouponCode() string {
+	const (
+		startHangul = 0xAC00 // '가'
+		endHangul   = 0xD7A3 // '힣'
+	)
+	randomHangulCodePoint := rune(rand.Intn(endHangul-startHangul+1) + startHangul)
+	randomNumberPart := rand.Intn(1_000_000_000)
+	return fmt.Sprintf("%c%09d", randomHangulCodePoint, randomNumberPart)
 }
