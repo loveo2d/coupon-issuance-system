6b292c632ec4ae9304bb6d709cc4f0e632c52bd2
loveo2d <loveo2d@naver.com>
2025-06-13 21:14:05 +0900

    add: db 추상화 인터페이스 추가

--- internal/infra/db/config.go
+++ internal/infra/db/config.go
@@ -3,6 +3,8 @@ package db
 import (
 	"context"
 
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -10,4 +12,10 @@ const databaseURL = "postgresql://postgres:postgres@localhost:5432/coupon_issuan
 
 func NewDB() (*pgxpool.Pool, error) {
 	return pgxpool.New(context.Background(), databaseURL)
+}
+
+type DB interface {
+	Exec(ctx context.Context, sql string, arguments ...any) (pgconn.CommandTag, error)
+	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
 }
